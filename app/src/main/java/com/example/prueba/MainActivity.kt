f2c8cd76f6ced1abe8bed36b969e08963cb0510c
Jesusuxul <i.211t0055@suryucatan.tecnm.mx>
2025-08-29 13:32:15 -0600

    dd

---
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.prueba
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val editEmail = findViewById<EditText>(R.id.editEmail)
+        val editPassword = findViewById<EditText>(R.id.editPassword)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val email = editEmail.text.toString().trim()
+            val password = editPassword.text.toString().trim()
+
+            when {
+                email.isEmpty() && password.isEmpty() -> {
+                    showDialog("Ambas casillas están vacías")
+                }
+                email.isEmpty() -> {
+                    showDialog("El campo correo está vacío")
+                }
+                password.isEmpty() -> {
+                    showDialog("El campo contraseña está vacío")
+                }
+                else -> {
+                    // Ambos llenos → pasar a segunda pantalla
+                    val intent = Intent(this, SegundaP::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+
+    private fun showDialog(message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Bienvenido")
+        builder.setMessage(message)
+        builder.setPositiveButton("✖") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+}
