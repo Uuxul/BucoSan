1f8fc31df383ba276de94caf2384c42f46ff916b
Jesusuxul <157435079+Uuxul@users.noreply.github.com>
2025-09-24 13:21:27 -0600

    Update

--- app/src/main/java/com/example/prueba/Config.kt
+++ app/src/main/java/com/example/prueba/Config.kt
@@ -1,7 +1,7 @@
 package com.example.prueba
 
 object Config {
-    
-    var BASE_URL = "http://192.168.1.74/AppBuco/"
 
+    //var BASE_URL = "http://192.168.1.74/AppBuco/"
+    var BASE_URL = "http://192.168.4.79/ProyectoBuco/"
 }
